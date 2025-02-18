7e7f2ab524958cff048078fbb5da744c26cc92b5
dust developer <jtgemarino22@src.pshs.edu.ph>
2025-02-18 08:54:23 +0800

    update to js
    Removed main() function and replaced it with step()
    
    additional html is implemented

--- main.js
+++ main.js
@@ -3,8 +3,10 @@ const CHARACTER_ARRAY = ['q','w','e','r','t','y','u','i','o','p','a','s','d','f'
 const MAX_HIT_COUNT = 5;
 
 let gameCompleted = false; 
-let targetWord = "Sword"; // the word to be solved in hangman
+let targetWord = "Sword".toLowerCase(); // the word to be solved in hangman
 
+let hitCount = 0;
+let correctCount = 0;
 
 // BIG NOTE: REMOVE ALL LOOPS IN THE CODE
 
@@ -17,79 +19,54 @@ function startAccomplisedEnd(){
 
 }
 
-function update(){
+function update(elementItem, Color){
 
 }
 
-function prepareGame(){ // dynamically add in the elements
-    for (let caIndex = 0; caIndex < CHARACTER_ARRAY.length; caIndex++){
-        let characterElement = document.createElement("div")
-        characterElement.setAttribute("id", ca)
-    }
-}
 // BACK-END FUNCTIONS -------------------------------------------------
 // .indexOf(a) < 0  : item was not found
 // .indexOf(a) >= 0 : item WAS found
 // note: console logs and alerts are not semi-colon'ed. please fix later
 
-function main(){
-    let hitCount = 0;
+function step(){ // stepping function to progress the game
+    let key = prompt("What's your character (e.g. a, b, c)?");
+    if (key.length > 1 && CHARACTER_ARRAY.indexOf(key) < 0){
+        alert("This isn't valid.")
+    }
+    // find the key in the document
+    let keyElement = document.getElementById(key)
 
-    let dissectedArray = targetWord.toLowerCase().split("");
-    let currentCharArray = [];
-   
-    while (!gameCompleted){
-        let key = "1234567890"; // random string so it won't bypass the check
+    if (targetWord.indexOf(key) < 0){
+        hitCount++;
+        // change the element css to red
+        console.log(hitCount)
+        console.log("COULD NOT FIND A HIT IN THE TARGETWORD! ALERTING.")
 
-        while (key.length > 1 && CHARACTER_ARRAY.indexOf(key) < 0){
-            key = prompt("What's your character (e.g. a, b, c)?");
-            if (!key){
-                key = "1234567890"
-                continue;
-            }
-        };
-        // this section assumes that the key is 1 and the key is on the CHAR_ARRAY
-        console.log("BASIC KEY-CHECK COMPLETE")
-
-        // if input is incorrent, add hitCount by 1
-        if (dissectedArray.indexOf(key) < 0){
-            hitCount++;
-            console.log(hitCount)
-            console.log("COULD NOT FIND A HIT IN THE DISSECTED ARRAY! ALERTING.")
-        } else if (currentCharArray.indexOf(key) < 0) {
-            currentCharArray.push(key);
-            console.log("FOUND A HIT IN THE DISSECTED ARRAY! ALERTING.")
-            alert("You've got a hit.")
-        } else {
-            hitCount++;
-            console.log(hitCount)
-            console.log("You've already got this correct answer. ALERTING.")
-            alert("You've already gotten this correct answer.")
-        };
-
-        // check if hitcount has reached max
-        if (hitCount >= MAX_HIT_COUNT){
-            gameCompleted = true;
-            console.log("GAME HAS COMPLETED. HITCOUNT HAS EXCEEDED MAX. ALERTING.")
-            alert("You've failed now.")
-            continue; // to end the game
-        };
-
-        if (currentCharArray.length < dissectedArray.length) {
-            console.log("CCA IS LESS THAN DA")
-            continue; // to bypass the last section of code and continue the game.
-                    // There's no need to check matching if theres \n
-                    // less elements in the current array than the dissected.
-        };
-
-        gameCompleted = true;
-        console.log("FINISH FINISH FINISSSSSHHHHHH!!!!!!")
-        alert("You've won. What now?")
-        continue; // to end the game
+    } else if (targetWord.indexOf(key) >= 0) {
+        correctCount++;
+        // change the element css to green
+        console.log("FOUND A HIT IN THE TARGETWORD! ALERTING.")
+    } else {
+        hitCount++;
+        console.log(hitCount)
+        console.log("You've already got this correct answer. ALERTING.")
     };
 
-    console.log("game ended!")
-    // main() // unused so the game can be debugged
-};
+    if (hitCount >= MAX_HIT_COUNT){
+        console.log("GAME HAS COMPLETED. HITCOUNT HAS EXCEEDED MAX. ALERTING.")
+        alert("You've failed now.")
+        gameCompleted = true;
+        return; // to end the game
+    };
 
-main()
+    if (correctCount < targetWord.length) {
+        console.log("CCA IS LESS THAN DA")
+        return; // to continue the game
+    };
+
+    console.log("FINISH FINISH FINISSSSSHHHHHH!!!!!!")
+    alert("You've won. What now?")
+    gameCompleted = true;
+    return; // to end the game
+}
+
